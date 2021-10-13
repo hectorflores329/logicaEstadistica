df637097886a8a0c4a29bc72c3385cda4c6bf9ed
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-10-13 14:25:07 -0600

    Commit

--- descarga.py
+++ descarga.py
@@ -1,14 +1,32 @@
 import pandas as pd
-import time
 import requests
+import pathlib
+import os
+import urllib
 
-def general():
+df = pd.read_excel('estadísticas-regionales-nuevo.xlsx', sheet_name='Links')
+
+def principal():
     descarga()
 
 def descarga():
-    # LÓGICA COMPLETA
-    print("HOLA")
+    
+    for i, index in df.iterrows():
+    
+    tema = df['Tema'][i]
+    nombre = df['Nombre'][i]
+    region = df['Región'][i]
+    url = df['Link'][i]
+        
+    ext = pathlib.Path(url)
+    extType = ext.suffix.split("?")
+    
+    # print(url)
+    # print(i+1)
+    
+    file = requests.get(url)
+    open('files/' + str(tema) + ' - ' + str(nombre) + ' - ' + str(region) + str(extType[0]), 'wb').write(file.content)
 
 
 if __name__ == '__main__':
-    general()
+    principal()
