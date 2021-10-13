0a971f652ce2124cf3f521b48cea2babc0720f78
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-10-13 14:18:18 -0600

    Commit

---
+++ descarga.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import time
+import requests
+
+def general():
+    descarga()
+
+def descarga():
+    # LÓGICA COMPLETA
+    print("HOLA")
+
+
+if __name__ == '__main__':
+    general()
